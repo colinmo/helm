a068937132bf3c5e88d71badc8ffc1dbdf6609ee
Colin Morris <relapse@gmail.com>
2022-08-11 07:58:37 +1000

    Build uptdate

--- src/search_test.go
+++ src/search_test.go
@@ -5,7 +5,8 @@ import (
 )
 
 func TestSearchDirectory(t *testing.T) {
-	charles, err := searchFiles(`f:\dropbox\swap\golang\helm\fixtures\search\a`, "search term")
+	//charles, err := searchFiles(`f:\dropbox\swap\golang\helm\fixtures\search\a`, "search term")
+	charles, err := searchFiles(`/Users/s457972/Dropbox/swap/golang/helm/fixtures/search/a`, "search term")
 	if err != nil {
 		t.Fatalf("Failed to execute at all %s\n", err)
 	}
