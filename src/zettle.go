47f7124f46e8cc33a702f0714db8c1cf266391f3
Colin Morris <c.morris@griffith.edu.au>
2024-08-21 10:52:29 +1000

    Renamed

--- src/zettle.go
+++ src/zettle.go
@@ -24,7 +24,7 @@ func moveZettleDate(hours time.Duration) string {
 func getFileContentsAndCreateIfMissing(filename string) string {
 	content, err := os.ReadFile(filename)
 	if errors.Is(err, os.ErrNotExist) {
-		content = []byte(fmt.Sprintf("---\nDate: %s\nName: %s\ntags: [\"status\"]\n---\n", AppStatus.CurrentZettleDBDate.Local().Format("2006-01-02"), AppStatus.CurrentZettleDBDate.Local().Format("2006-01-02")))
+		content = []byte(fmt.Sprintf("---\nDate: %s\nName: Status - %s\ntags: [\"status\"]\n---\n", AppStatus.CurrentZettleDBDate.Local().Format("2006-01-02"), AppStatus.CurrentZettleDBDate.Local().Format("2006-01-02")))
 		os.WriteFile(filename, content, 0666)
 	}
 	return string(content)
